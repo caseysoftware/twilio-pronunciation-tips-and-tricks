a0c196792013579d1244416e69671259a3cfa6df
D Keith Casey Jr <keith@caseysoftware.com>
2013-07-10 22:44:54 -0500

    re-enabled the female voice

--- heteronyms-corrected.php
+++ heteronyms-corrected.php
@@ -22,7 +22,7 @@ $response = new Services_Twilio_Twiml();
 
 foreach($items as $item) {
     $response->say($item);
-    //$response->say($item, array('voice' => 'woman'));
+    $response->say($item, array('voice' => 'woman'));
     $response->say($item, array('voice' => 'alice'));
 }
 
