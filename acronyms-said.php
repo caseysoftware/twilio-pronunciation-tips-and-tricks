71a2f9486e4213275a97f3c741befa805c721592
D Keith Casey Jr <keith@caseysoftware.com>
2013-08-04 17:09:45 -0500

    updated the sample to include the recording

--- acronyms-said.php
+++ acronyms-said.php
@@ -3,14 +3,18 @@
 include 'Services/Twilio.php';
 
 /**
- * POTUS is the oddity
+ * All of the acronyms are pronounced correctly by both voices except for
+ *   the pretty rare acronym: POTUS. Alice pronounces it correctly while the
+ *   woman voice spells it. You can listen to the recording here:
+ *
+ * https://soundcloud.com/caseysoftware/twilio-tts-for-pronounced
  */
 $items = array('NASA', 'NATO', 'AIDS', 'SCUBA', 'laser', 'POTUS');
 
 $response = new Services_Twilio_Twiml();
 
 foreach($items as $item) {
-    $response->say($item);
+    $response->pause();
     $response->say($item, array('voice' => 'woman'));
     $response->say($item, array('voice' => 'alice'));
 }
