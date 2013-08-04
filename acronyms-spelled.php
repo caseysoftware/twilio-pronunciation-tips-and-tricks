50976893274c20a916bc25023b56f4f39511b7e7
D Keith Casey Jr <keith@caseysoftware.com>
2013-08-04 17:05:15 -0500

    updated the sample to handle recording

--- acronyms-spelled.php
+++ acronyms-spelled.php
@@ -3,14 +3,18 @@
 include 'Services/Twilio.php';
 
 /**
- * UN is the oddity
+ * All of the acronyms are spelled correctly by both voices except for UN. Alice
+ *   pronounces it correctly while the woman voice says "un" as in "fun". You
+ *   can listen to the recording here:
+ *
+ * https://soundcloud.com/caseysoftware/twilio-voices-for-spelled
  */
 $items = array('CIA', 'NBC', 'CNN', 'HTTP', 'http', 'UTC', 'UN');
 
 $response = new Services_Twilio_Twiml();
 
 foreach($items as $item) {
-    $response->say($item);
+    $response->pause();
     $response->say($item, array('voice' => 'woman'));
     $response->say($item, array('voice' => 'alice'));
 }
