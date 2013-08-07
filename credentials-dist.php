9c268d0c486a76523876097079a07ff87f39c463
D Keith Casey Jr <keith@caseysoftware.com>
2013-08-07 17:45:39 -0500

    added a sample config to get people started

---
+++ credentials-dist.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Rename this file to credentials.php and fill in the appropriate values below.
+ */
+
+$accountSid = 'ACxxxx';
+$authToken  = '123456';
+
+$callFrom   = '512-555-1212';
+$callTo     = '703-555-1212';
+
+$myUrlHostingTheSaySamples = 'http://example.com/';
